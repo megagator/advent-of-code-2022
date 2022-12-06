d03c2349f1592eee66126c6f7f32db9a9410752f
Mike Reinhard <mike@mikereinhard.com>
2022-12-05 20:13:20 -0800

    day 5

---
+++ day 5/supply_stacks_p1.py
@@ -0,0 +1,44 @@
+from pprint import pprint
+import re
+
+
+with open('input.txt') as f:
+    content = f.read().splitlines()
+
+crates = []
+moves = []
+
+instruction_stage = 'crates'
+crate_regex = r"(\[[A-Z]\]\s?|\s{4})?"
+move_regex = r"move (\d+) from (\d+) to (\d+)"
+
+for line in content:
+    if line == '':
+        instruction_stage = 'moves'
+
+    if instruction_stage == 'crates':
+        # the crate map:
+        matches = re.finditer(crate_regex, line)
+        if matches:
+            for match_num, match in enumerate(matches, start=1):
+                m = match.group(0).strip()
+                if m != '':
+                    while len(crates) <= match_num:
+                        crates.append([])
+                    
+                    crate = re.sub(r'[\[\]\s]', '', m)
+                    crates[match_num].insert(0, crate)
+    else:
+        # the move instructions:
+        match = re.match(move_regex, line)
+        if match is not None:
+            amount, source, destination = [int(m) for m in match.groups()]
+            moves.append([amount, source, destination])
+
+for move in moves:
+    amount, source, destination = move
+    for i in range(amount):
+        crates[destination].append(crates[source].pop())
+
+top_crates = ''.join([c.pop() for c in crates if len(c) > 0])
+print(top_crates)
